cc2515987c6c1e1d01743a0bcc15860c676fd555
geofrey <devongeoffreymaina@gmail.com>
2024-10-27 11:41:49 +0300

    Added django-environ

--- magnetobackend/settings.py
+++ magnetobackend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u&gfb!s_m$s^@m+2)i+pf&39cl)$*f)h(b_#!&0@5^ewtl+x^+'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
